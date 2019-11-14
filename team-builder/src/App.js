6b701ed928f3ec7d318bb854996ed279e67e968c
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-11-14 06:48:33 +0000

    Code clean up

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Form from './Components/Form';
 
 
 function App() {
-  // const [teamMembers, setTeamMembers] = useState([{}]);
-//   const [memberDetails, setMemberDetails] = useState({
-//     name: "",
-//     email: "",
-//     role: "",
-// });
 
   return (
     <div className="App">
-      <Form 
-      // teamMembers={teamMembers}
-      // setTeamMembers={setTeamMembers}
-      // memberDetails={memberDetails}
-      // setMemberDetails={setMemberDetails}
-      />
+      <Form />
     </div>
   );
 }
--- team-builder/src/Components/Form.js
+++ team-builder/src/Components/Form.js
@@ -5,7 +5,6 @@ import MemberList from './MemberList';
 
 function Form (props){
 
-  // let teamMembers = [];
   const [teamMembers, setTeamMembers] = useState([
     {
     name: 'Emma',
@@ -17,23 +16,9 @@ function Form (props){
     email: 'Matt@hotmail.com',
     role: 'Full-Stack Developer'
   }])
-//   const [memberDetails, setMemberDetails] = useState({
-//     name: "",
-//     email: "",
-//     role: "",
-// });
-
-  //  const handleFormChange = e => {
-  //       if(/^[A-Za-z]+$/.test(e.target.value) || e.target.value === "") {
-  //           setMemberDetails({
-  //               ...memberDetails,
-  //               [e.target.name]: e.target.value})
-  //       }
-  //     }
       
   const handleSubmit = e => {
     e.preventDefault();
-    // e.persist();
     const newMember = {
       name: e.target.name.value,
       email: e.target.email.value,
@@ -41,38 +26,6 @@ function Form (props){
     }
     setTeamMembers(teamMembers.concat(newMember))
   }
-
-  // const handleArrayChange = e => {
-  //   console.log('array change', membersList);
-  //       membersList.map((curr, index) => {
-  //         return (
-  //           <div key={index}>
-  //             <MemberList 
-  //               key={index}
-  //               curr={curr}/>
-  //           </div>
-  //         )
-  //       })
-  // }
-
-
-// console.log(teamMembers);
-// console.log(memberDetails);
-
-    // const handleSubmit = e => {
-    //     e.preventDefault();
-    //     console.log(props.memberDetails);
-    //     const newMember = {
-    //     name: e.target.name.value,
-    //     email: e.target.email.value,
-    //     role: e.target.role.value
-    //     }
-
-    //     props.setMemberDetails(props.memberDetails.concat(newMember))
-
-        // membersList.push(newMember);
-        // props.setMemberDetails(props.memberDetails.concat(newMember))
-      // };
 
         return (
             <div>
@@ -83,7 +36,6 @@ function Form (props){
                         type="text"
                         name="name"
                         placeholder="Enter your name here" 
-                        // onChange={handleFormChange}
                        /> 
                     </label>
                     <label>email: 
@@ -91,14 +43,12 @@ function Form (props){
                         type="email"
                         name="email"
                         placeholder="Enter your email here" 
-                        // onChange={handleFormChange}
                         /> 
                     </label>
                     <label>Role: 
                       <select
                       type="text"
                       name="role"
-                      // onChange={handleFormChange}
                       >
                         <option></option>
                         <option>UI</option>
